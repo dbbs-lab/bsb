d661f031e2d75738c2da10f0f5fb666466941c94
drodarie <d.rodarie@gmail.com>
2025-04-17 18:02:58 +0200

    feat: add bsb-json

---
+++ packages/bsb-json/tests/test_configuration.py
@@ -0,0 +1,57 @@
+import json
+import unittest
+from tempfile import TemporaryFile
+
+from bsb import (
+    Configuration,
+    Scaffold,
+    config,
+    parse_configuration_content,
+    parse_configuration_file,
+)
+from bsb_test import RandomStorageFixture, get_test_config_tree
+
+
+def as_json(name: str):
+    import json
+
+    return json.dumps(get_test_config_tree(name))
+
+
+class TestConfiguration(
+    RandomStorageFixture, unittest.TestCase, setup_cls=True, engine_name="fs"
+):
+    def test_default_bootstrap(self):
+        cfg = config.Configuration.default(storage={"engine": "fs"})
+        Scaffold(cfg, self.storage)
+
+    def test_json_minimal_bootstrap(self):
+        with TemporaryFile(mode="w+") as f:
+            f.write(as_json("minimal"))
+            f.seek(0)
+            config = parse_configuration_file(f, parser="json")
+        Scaffold(config, self.storage)
+
+    def test_json_minimal_content_bootstrap(self):
+        config = parse_configuration_content(as_json("minimal"), parser="json")
+        Scaffold(config, self.storage)
+
+    def test_json_full_bootstrap(self):
+        with TemporaryFile(mode="w+") as f:
+            f.write(as_json("full_compile"))
+            f.seek(0)
+            config = parse_configuration_file(f, parser="json")
+        Scaffold(config, self.storage)
+
+    def test_full_bijective(self):
+        self.bijective(
+            "full_compile", Configuration, get_test_config_tree("full_compile")
+        )
+
+    def bijective(self, name, cls, tree):
+        # Test that the tree and its config projection are the same in JSON
+        with self.subTest(name=name):
+            cfg = cls(tree)
+            new_tree = cfg.__tree__()
+            self.assertEqual(json.dumps(tree, indent=2), json.dumps(new_tree, indent=2))
+            return cfg, new_tree
---
+++ packages/bsb-json/tests/test_schema.py
@@ -0,0 +1,24 @@
+import unittest
+
+from bsb import config
+
+from bsb_json import get_schema
+
+
+def test_empty_schema():
+    class X:
+        pass
+
+    schema = get_schema(X)
+    assert schema["type"] == "object"
+    assert schema["properties"] == {}
+    assert schema["title"] == "Configuration"
+    assert schema["$defs"] == {}
+
+
+def test_schema_attrs():
+    class X:
+        y = config.attr()
+
+    schema = get_schema(X)
+    print(schema)

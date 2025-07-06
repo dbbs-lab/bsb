4bdb90b58a65a5ab6006001efe48d2dd5bcd328a
drodarie <d.rodarie@gmail.com>
2025-07-06 09:16:21 +0200

    fix: move sed commands related to codemeta.json from main.yml into a python script.

---
+++ devtools/update_codemeta.py
@@ -0,0 +1,30 @@
+"""Utility script to update the codemeta.json file."""
+
+import datetime
+from os.path import dirname, join
+import re
+import json
+
+project_folder = dirname(__file__)
+
+with open(join(project_folder, "pyproject.toml"), "r") as f:
+    for line in f.readlines():
+        if line.startswith("version = "):
+            version = line.split('version = "')[1].rsplit('"')[0]
+            break
+    else:
+        raise Exception("Could not find version in pyproject.toml")
+
+codemeta_file = join(dirname(dirname(project_folder)), "codemeta.json")
+with open(codemeta_file, "r+") as f:
+    metadata = json.loads(f.read())
+    metadata["dateModified"] = str(datetime.datetime.now().date())
+    metadata["version"] = version
+    metadata["downloadUrl"] = re.sub(
+        r"/tags/v(\.?[0-9]+){3}", r"/tags/v" + version, metadata["downloadUrl"]
+    )
+    metadata["releaseNotes"] = re.sub(
+        r"/tag/v(\.?[0-9]+){3}", r"/tag/v" + version, metadata["releaseNotes"]
+    )
+    f.seek(0)
+    json.dump(metadata, f, indent=4)

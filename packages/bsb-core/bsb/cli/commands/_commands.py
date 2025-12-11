875c5e4be37df33314d9858f0ebfaf83500d5b21
drodarie <d.rodarie@gmail.com>
2025-12-11 14:11:46 +0100

    fix: add message explaining `exists` flag

--- packages/bsb-core/bsb/cli/commands/_commands.py
+++ packages/bsb-core/bsb/cli/commands/_commands.py
@@ -220,7 +220,9 @@ class BsbSimulate(BaseCommand, name="simulate"):
                 )
             except FileExistsError:
                 return report(
-                    f"Could not create '{root.absolute()}', directory exists.", level=0
+                    f"Could not create '{root.absolute()}', directory exists. "
+                    "Use flag '--exists' to ignore this error. ",
+                    level=0,
                 )
         MPI.barrier()
         try:
